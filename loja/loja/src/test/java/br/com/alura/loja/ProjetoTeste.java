8176f704fd8e8985e11a8bb7d8605ac042f203b7
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-26 17:03:06 -0300

    Teste Java - automatizando e deserializando o Teste do Modelo (Projeto)

--- loja/loja/src/test/java/br/com/alura/loja/ProjetoTeste.java
+++ loja/loja/src/test/java/br/com/alura/loja/ProjetoTeste.java
@@ -4,11 +4,29 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Projeto;
 import junit.framework.Assert;
 
 public class ProjetoTeste {
+	
+	private HttpServer servidor;
+
+	@Before
+	public void startaServidor() {
+		this.servidor = Servidor.inicializaServidor();
+	}
+	
+	@After
+	public void stopServidor() {
+		servidor.stop();
+	}
 
 	@Test
 	public void testaConexaoDoResourceProjeto() {
@@ -17,6 +35,8 @@ public class ProjetoTeste {
 		WebTarget target = client.target("http://localhost:8080");
 		String conteudo = target.path("/projetos").request().get(String.class);
 		
-		Assert.assertTrue(conteudo.contains("Minha loja"));
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		
+		Assert.assertEquals("Minha loja", projeto.getNome());
 	}
 }
