16d6484bf289ae3d839edcb2592002dc632d28cc
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-26 16:55:39 -0300

    Teste Java - automatizando o teste de carrinho para automaticamente dar um start e stop no server

--- loja/loja/src/main/java/br/com/alura/loja/Servidor.java
+++ loja/loja/src/main/java/br/com/alura/loja/Servidor.java
@@ -12,13 +12,18 @@ public class Servidor {
 
 	public static void main(String[] args) throws IOException {
 		
-		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
-		
-		URI uri = URI.create("http://localhost:8080/");
-		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		HttpServer server = inicializaServidor();
 		System.out.println("Servidor Rodando");
 		System.in.read();
 		server.stop();
 		
+	}
+
+	public static HttpServer inicializaServidor() {
+		ResourceConfig config = new ResourceConfig().packages("br.com.alura.loja");
+		
+		URI uri = URI.create("http://localhost:8080/");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		return server;
 	}
 }
--- loja/loja/src/test/java/br/com/alura/loja/CarrinhoTest.java
+++ loja/loja/src/test/java/br/com/alura/loja/CarrinhoTest.java
@@ -4,14 +4,29 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
 
-public class ClientTest {
+public class CarrinhoTest {
+	
+	private HttpServer server;
+
+	@Before
+    public void before() {
+        this.server = Servidor.inicializaServidor();
+    }
+
+    @After
+    public void mataServidor() {
+        server.stop();
+    }
 	
 	@Test
 	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
