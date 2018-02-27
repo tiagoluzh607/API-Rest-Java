a13e4f5711f2b906a6154a43cc3643f7e8da5b9b
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-27 10:09:45 -0300

    Carrinho Resource - Refatorando parametros passando direto na URL o id - melhoria para conseguir performance de cache

--- loja/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -2,6 +2,7 @@ package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -12,9 +13,10 @@ import br.com.alura.loja.modelo.Carrinho;
 @Path("carrinhos")
 public class CarrinhoResource {
 
+	@Path("{id}") //indica a url como /id no final /carrinhos/id
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public String busca(@QueryParam("id") long id) { //acessar localhost:8080/carrinhos?id=1
+	public String busca(@PathParam("id") long id) { //acessar localhost:8080/carrinhos/1
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		return carrinho.toXML();
 	}
