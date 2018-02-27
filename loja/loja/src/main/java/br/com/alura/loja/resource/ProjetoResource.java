7bab965c2f772a8a933aa7562c029eff8312b841
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-27 10:22:08 -0300

    Projeto Resource - Refatorado para receber filtro doreto na URI e retornar o modelo(Projeto)

--- loja/loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ loja/loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -2,6 +2,7 @@ package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,12 +12,13 @@ import br.com.alura.loja.modelo.Projeto;
 @Path("projetos")
 public class ProjetoResource {
 	
+	@Path("{id}") // acessar com /id no final do link
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public String busca() {
+	public String busca(@PathParam("id") long id) { // acessar localhost:8080/projetos/1
 		
 		ProjetoDAO projetoDAO = new ProjetoDAO();
-		Projeto projeto = projetoDAO.busca(1l);
+		Projeto projeto = projetoDAO.busca(id);
 		return projeto.toXML();
 	}
 
