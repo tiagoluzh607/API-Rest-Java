2cd174d8f1e279694ed185b6cf3a21564be4fa89
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-27 11:50:22 -0300

    Teste Java - Implementação para o teste do Post de Carrinho

--- loja/loja/src/test/java/br/com/alura/loja/CarrinhoTest.java
+++ loja/loja/src/test/java/br/com/alura/loja/CarrinhoTest.java
@@ -2,7 +2,10 @@ package br.com.alura.loja;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -13,6 +16,7 @@ import org.junit.Test;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 public class CarrinhoTest {
 	
@@ -41,6 +45,27 @@ public class CarrinhoTest {
 		//Verifica a rua do objeto carrinho
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 		
+	}
+	
+	@Test
+	public void testaAdicionarUmNovoCarrinho() {
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		
+		//criando carrinho
+		Carrinho carrinho = new Carrinho();
+        carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Sao Paulo");
+        
+        String carrinhoXML = carrinho.toXML();
+        
+        //Enviando via post
+        Entity<String> entity = Entity.entity(carrinhoXML, MediaType.APPLICATION_XML);
+
+        Response response = target.path("/carrinhos").request().post(entity);
+        Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
 	}
 
 }
