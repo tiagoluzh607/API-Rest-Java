a75070712e39746dde105b7fb7f3c2b637c2a656
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-27 12:06:42 -0300

    Projetos Resource - Adicionando Projetos via Post

--- loja/loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ loja/loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -1,10 +1,13 @@
 package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.ProjetoDAO;
 import br.com.alura.loja.modelo.Projeto;
@@ -20,6 +23,15 @@ public class ProjetoResource {
 		ProjetoDAO projetoDAO = new ProjetoDAO();
 		Projeto projeto = projetoDAO.busca(id);
 		return projeto.toXML();
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String adiciona(String conteudo) { //mandar um projeto xml via post para localhost:8080/projetos
+		
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		new ProjetoDAO().adiciona(projeto);
+		return "<status>sucesso</status>";
 	}
 
 }
