d6bd4c8de6ee56c0c94ab4619a50d3ab99bbab69
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-27 11:19:43 -0300

    Carrinhos Resource - Adicionando Carrinhos via Post

--- loja/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -1,11 +1,14 @@
 package br.com.alura.loja.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
@@ -19,5 +22,13 @@ public class CarrinhoResource {
 	public String busca(@PathParam("id") long id) { //acessar localhost:8080/carrinhos/1
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		return carrinho.toXML();
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_XML)
+	public String adiciona(String conteudo) { //mandar um carrinho xml via post para localhost:8080/carrinhos
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		new CarrinhoDAO().adiciona(carrinho);
+		return "<status>sucesso</status>";
 	}
 }
