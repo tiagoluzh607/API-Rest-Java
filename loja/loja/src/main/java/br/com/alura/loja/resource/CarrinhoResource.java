a16b7f663ed6674886d4249061222baca4c0b966
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-28 12:02:46 -0300

    Carrinho Resource - Alterando um produto inteiro dentro de um carrinho

--- loja/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ loja/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,6 +18,7 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -41,11 +43,22 @@ public class CarrinhoResource {
 		return Response.created(uri).build(); //retorna uma o código http de sucesso e manda a url onde se encontra o recurso, esse caminho cai no header da resposta [Location: http://localhost:8080/carrinhos/2]
 	}
 	
-	@Path("{id}/produtos/{produtoId}")
+	@Path("{id}/produtos/{produtoId}") // acessar via DELETE exemplo localhost:8080/carrinhos/1/produtos/3467
 	@DELETE
 	public Response removeProduto(@PathParam("id") long id, @PathParam("produtoId") long produtoId) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		carrinho.remove(produtoId);
+		return Response.ok().build();
+	}
+	
+	@Path("{id}/produtos/{produtoId}") //// acessar via PUT passando um xml exemplo localhost:8080/carrinhos/1/produtos/3467
+	@PUT
+	@Consumes(MediaType.APPLICATION_XML) //indica que só aceitamos no http o media type xml
+	public Response alteraProduto(String conteudo, @PathParam("id") long id, @PathParam("produtoId") long produtoId) {
+		
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		Produto produto = (Produto) new XStream().fromXML(conteudo);
+		carrinho.troca(produto);
 		return Response.ok().build();
 	}
 }
