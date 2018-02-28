45f77977b1b9251530a5db4e32c3867ff8afd5e5
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-02-28 10:48:07 -0300

    Projeto Resouce - Implementação do DELETE de um projeto

--- loja/loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ loja/loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -3,6 +3,7 @@ package br.com.alura.loja.resource;
 import java.net.URI;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -19,7 +20,7 @@ import br.com.alura.loja.modelo.Projeto;
 @Path("projetos")
 public class ProjetoResource {
 	
-	@Path("{id}") // acessar com /id no final do link
+	@Path("{id}") // acessar com /id no final do link exemplo localhost:8080/projetos/1
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
 	public String busca(@PathParam("id") long id) { // acessar localhost:8080/projetos/1
@@ -38,6 +39,13 @@ public class ProjetoResource {
 		
 		URI uri = URI.create("/projetos/"+projeto.getId()); //cria uma uri com o local do novo recurso criado
 		return Response.created(uri).build(); //retorna uma o código http de sucesso e manda a url onde se encontra o recurso, esse caminho cai no header da resposta Ex: [Location: http://localhost:8080/projetos/2]
+	}
+	
+	@Path("{id}") // acessar /id no final da url exemplo localhost:8080/projetos/1
+	@DELETE
+	public Response remove(@PathParam("id") long id) {
+		new ProjetoDAO().remove(id); // remove o projeto
+		return Response.ok().build(); // retorna o status code 200 ok
 	}
 
 }
