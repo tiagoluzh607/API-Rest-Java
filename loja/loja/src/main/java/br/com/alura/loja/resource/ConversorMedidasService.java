c6d3154557ab6485adc56cefbafbccfcbe17f902
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-03-02 18:01:39 -0300

    Feitas Classes de Conversão(Serviço normal)

---
+++ loja/loja/src/main/java/br/com/alura/loja/resource/ConversorMedidasService.java
@@ -0,0 +1,30 @@
+package br.com.alura.loja.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+public class ConversorMedidasService {
+
+	
+	
+	@GET //Defini qual o verbo http que vai utilizado pra chamar esse método
+	@Path("quilometrosParaMilhas/{quilometros}")//Defini qual vai ter o url pra acessar o método, sendo o {quilometros} um parâmetro que é quantidade de quilometro que vai ser convertido pra milhas
+	public Response quilometroParaMilha(@PathParam("quilometros")Double quilometros){ 	//Metodo que faz um simples conversão de quilometro para milhas
+			
+		quilometros = quilometros / 1.6;
+			return Response.ok(quilometros).build();
+	}
+	
+	@GET
+	@Path("milhasParaQuilometros/{milhas}")
+	//Metodo que faz um simples conversão de milhas para quilometros
+	public Response milhasParaQuilometros(@PathParam("milhas")Double milhas)
+	{
+			milhas = milhas * 1.6;
+
+			return Response.ok(milhas).build();
+	}
+
+}
