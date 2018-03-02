d198cb0ad56028baeffc20301094f46df15cf370
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-03-02 18:08:50 -0300

    Criado um Modelo chamado Credencial

---
+++ loja/loja/src/main/java/br/com/alura/loja/modelo/Credencial.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja.modelo;
+
+public class Credencial {
+	private String login;
+	private String senha;
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
--- loja/loja/src/main/java/br/com/alura/loja/resource/ConversorMedidasService.java
+++ loja/loja/src/main/java/br/com/alura/loja/resource/ConversorMedidasService.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+@Path("servicos")
 public class ConversorMedidasService {
 
 	
