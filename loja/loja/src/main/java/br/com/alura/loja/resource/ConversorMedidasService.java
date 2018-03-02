d13a6b1c08593015f9bbf3181db2b0e40c0ce5af
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-03-02 19:50:07 -0300

    Colocando a anotação @Seguro nos métodos que serão protegidos

--- loja/loja/src/main/java/br/com/alura/loja/resource/ConversorMedidasService.java
+++ loja/loja/src/main/java/br/com/alura/loja/resource/ConversorMedidasService.java
@@ -5,11 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import br.com.alura.loja.seguranca.Seguro;
+
 @Path("servicos")
 public class ConversorMedidasService {
 
 	
-	
+	@Seguro // Nos criamos nao deixa executar sem autenticacao
 	@GET //Defini qual o verbo http que vai utilizado pra chamar esse método
 	@Path("quilometrosParaMilhas/{quilometros}")//Defini qual vai ter o url pra acessar o método, sendo o {quilometros} um parâmetro que é quantidade de quilometro que vai ser convertido pra milhas
 	public Response quilometroParaMilha(@PathParam("quilometros")Double quilometros){ 	//Metodo que faz um simples conversão de quilometro para milhas
@@ -18,6 +20,7 @@ public class ConversorMedidasService {
 			return Response.ok(quilometros).build();
 	}
 	
+	@Seguro // Nos criamos nao deixa executar sem autenticacao
 	@GET
 	@Path("milhasParaQuilometros/{milhas}")
 	//Metodo que faz um simples conversão de milhas para quilometros
